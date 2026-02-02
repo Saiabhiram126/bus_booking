49ba2f36cc4abe205ad48d1295fc3d4fc9a0687d
Sai Abhiram <132103875+Saiabhiram126@users.noreply.github.com>
2026-02-02 15:23:37 +0000

    Add explicit OPTIONS CORS handling

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: false
 }));
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
